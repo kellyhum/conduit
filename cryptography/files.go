d23ffd5450b0ad05f8a378123804b7e85e727945
Kelly Hum <kellyhum88@gmail.com>
2026-08-10 22:47:33 -0700

    remove unused function

--- cryptography/files.go
+++ cryptography/files.go
@@ -5,25 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"io"
-	"os"
 )
-
-// note: this might crash for rlly big programs b/c the bytestream is
-// going to be rlly big - could switch to piping it directly
-// from input -> tls after
-func EncryptFileFromDisk(path string, key []byte) ([]byte, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	encryptedData, err := EncryptData(data, key)
-	if err != nil {
-		return nil, err
-	}
-
-	return encryptedData, nil
-}
 
 func EncryptData(data []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
