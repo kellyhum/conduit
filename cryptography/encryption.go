0e5f931bfbab193bcabc23119e0b37c927f34381
Kelly Hum <kellyhum88@gmail.com>
2026-08-19 23:29:16 -0700

    add decryption and key getting funcs

--- cryptography/encryption.go
+++ cryptography/encryption.go
@@ -4,8 +4,8 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/ecdh"
+	"crypto/hkdf"
 	"crypto/rand"
-	"crypto/sha1"
 	"crypto/sha256"
 	"fmt"
 	"io"
@@ -33,4 +33,53 @@ func EncryptData(data []byte, key []byte) ([]byte, error) {
 	}
 
 	return gcm.Seal(nonce, nonce, data, nil), nil
+}
+
+func DecryptData(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < gcm.NonceSize() {
+		return nil, fmt.Errorf("ciphertext too short, currently at %d bytes but requires min %d bytes", len(data), gcm.NonceSize())
+	}
+
+	nonce := data[:gcm.NonceSize()]
+	ciphertext := data[gcm.NonceSize():]
+
+	// decrypt + proves data wasn't tampered with
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func GetSharedAESKey(privKey []byte, pubKey []byte) ([]byte, error) {
+	// rebuild keys
+	curve := ecdh.X25519()
+	privateKey, err := curve.NewPrivateKey(privKey)
+	if err != nil {
+		return nil, fmt.Errorf("private key error: %w", err)
+	}
+	publicKey, err := curve.NewPublicKey(pubKey)
+	if err != nil {
+		return nil, fmt.Errorf("public key error: %w", err)
+	}
+
+	// compute shared key
+	sharedKey, err := privateKey.ECDH(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// key derivation func (hkdf), resehape key to 32 bytes
+	aesKey, err := hkdf.Key(sha256.New, sharedKey, nil, "conduit-file-encryption", 32)
+	if err != nil {
+		return nil, err
+	}
+
+	return aesKey, nil
 }
